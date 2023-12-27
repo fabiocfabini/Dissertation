bd8577568b0f973a59c9455063eda388fc6ba855
fabiocfabini <fabiolucas.carneiro@gmail.com>
2023-12-27 19:37:38 +0000

    Add echos to build script

--- build.sh
+++ build.sh
@@ -1,14 +1,18 @@
 #!/bin/bash
 
-set -xe
-
 cd msc_latex_template
 
+echo "RUNNING: xelatex dissertation.tex"
 xelatex dissertation.tex
+echo "RUNNING: bibtex dissertation.aux"
 bibtex dissertation.aux
+echo "RUNNING: makeindex dissertation"
 makeindex dissertation
+echo "RUNNING: makeglossaries dissertation"
 makeglossaries dissertation
+echo "RUNNING: xelatex dissertation.tex"
 xelatex dissertation.tex
-mv dissertation.pdf ../
+
+mv dissertation.pdf ..
 
 cd ..
