fa9b8d5ee07a88f1c2ac61bdd2d6a34b56f21d77
fabiocfabini <fabiolucas.carneiro@gmail.com>
2024-01-07 22:57:23 +0000

    Post revision

--- clean.sh
+++ clean.sh
@@ -2,6 +2,9 @@
 
 echo "Cleaning workspace..."
 
-git clean -fdx -e *.zip
+cd msc_latex_template
+rm *.tdo *.acn *.aux *.glo *.glsdefs *.idx *.ilg *.ind *.ist *.lof *.log *.lot *.out *.toc *.bbl *.blg *.acr *.alg *.glg *.gls
+cd ..
+rm dissertation.pdf
 
 echo "Done."
